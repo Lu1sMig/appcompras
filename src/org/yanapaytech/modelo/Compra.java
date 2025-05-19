facf6e4951a7d92e5ccaba22f26b28e13e12e2e7
Lu1sMig <lmcavero.a1@gmail.com>
2025-05-18 21:35:09 -0500

    segundo commit

--- src/org/yanapaytech/modelo/Compra.java
+++ src/org/yanapaytech/modelo/Compra.java
@@ -1,4 +1,26 @@
 package org.yanapaytech.modelo;
-
-public class compra {
+public class Compra implements Comparable<Compra> {
+    private double valor;
+    private String descripcion;
+    //Constructor
+    public Compra(double valor, String descripcion){
+        this.valor = valor;
+        this.descripcion = descripcion;
+    }
+    //getter
+    public double getValor(){
+        return valor;
+    }
+    public String getDescripcion(){
+        return descripcion;
+    }
+    @Override
+    public String toString(){
+        return "Compra: valor= " + valor +
+                ", descripcion='" + descripcion;
+    }
+    @Override
+    public int compareTo(Compra otraCompra){
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(otraCompra.getValor()));
+    }
 }
